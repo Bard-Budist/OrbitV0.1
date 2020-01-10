c8644c1a864f37e96168548bcc5fa0266b5ef0a3
GiovanniAyala <op7puro@live.com>
2020-01-09 22:32:49 -0500

    Add DATA file and new API for testing

--- testSpeech.py
+++ testSpeech.py
@@ -1,5 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import speech_recognition as sr
+from gtts import gTTS
+import os
+
+mytext = 'Hi, Giovanni'
+language = 'en'
+
+
 
 r = sr.Recognizer()
 with sr.Microphone() as source:
@@ -8,5 +15,9 @@ with sr.Microphone() as source:
     try:
         text = r.recognize_google(audio)
         print("You said : {}".format(text))
+        if format(text) == "hello":
+            myobj = gTTS(text=mytext, lang=language, slow=False)
+            myobj.save("welcome.mp3")
+            os.system("play welcome.mp3")
     except:
         print("Sorry could not recognize what you said")
